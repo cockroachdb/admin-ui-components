11899acc6ac43d5133dacb74642977df75e5a391
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-08-12 13:18:48 -0400

    NotificationMessage component prototype
    Adding NotificationMessage component. At first I had a Notification
    component that could render any "type" of notification, but changed
    directions to have this component be presentational and render all
    notifications. I have stubbed the idea of "notification types" and the
    data for individual notifications in a `notification` directory. I will
    look to replace this with logic that will live in the redux store and
    probably be handled by a container component later.

---
+++ src/NotificationMessage/NotificationMessage.stories.tsx
@@ -0,0 +1,70 @@
+import React, { FunctionComponent, ReactNode } from "react";
+
+import { NotificationProps, generateNotificationProps } from "../Notifications";
+import { NotificationMessage } from "./index";
+
+export default {
+  title: "Notfication Message",
+  component: NotificationMessage,
+};
+
+const testNotifications: Array<NotificationProps> = [
+  {
+    id: 1,
+    type: "backup-blocked",
+    timestamp: "30 JUL 2020 13:22",
+    read: true,
+  },
+  {
+    id: 2,
+    type: "command-commit",
+    timestamp: "30 JUL 2020 13:25",
+    read: false,
+  },
+  {
+    id: 3,
+    type: "expired",
+    timestamp: "30 JUL 2020 13:30",
+    read: false,
+  },
+  {
+    id: 4,
+    type: "full-table",
+    timestamp: "30 JUL 2020 13:35",
+    read: true,
+  },
+  {
+    id: 5,
+    type: "expired",
+    timestamp: "30 JUL 2020 14:35",
+    read: true,
+  },
+  {
+    id: 6,
+    type: "network-partition",
+    timestamp: "12 AUG 2020 10:00",
+    read: false,
+  },
+];
+
+const notificationMessages = generateNotificationProps(testNotifications);
+
+const NotificationFrame: FunctionComponent<{
+  children: ReactNode;
+}> = ({ children }) => (
+  <section style={{ width: "30rem", padding: "2rem" }}>{children}</section>
+);
+
+export const Demo = () => (
+  <section>
+    <h1>Notification Types</h1>
+    {notificationMessages.map(n => (
+      <section key={n.id}>
+        <code>{n.type}</code>
+        <NotificationFrame>
+          <NotificationMessage {...n} />
+        </NotificationFrame>
+      </section>
+    ))}
+  </section>
+);
